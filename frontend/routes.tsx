5b4240ac6f340780f9356ff9682c33f2715c04ad
Ádám Jakab <adam.jakab@lombiq.com>
2024-01-21 23:17:16 +0100

    Changing routing

--- frontend/routes.tsx
+++ frontend/routes.tsx
@@ -1,28 +1,31 @@
 import Placeholder from 'Frontend/components/placeholder/Placeholder.js';
 import AuthControl from 'Frontend/views/AuthControl.js';
-import HelloWorldView from 'Frontend/views/helloworld/HelloWorldView.js';
 import MainLayout from 'Frontend/views/MainLayout.js';
 import LoginView from 'Frontend/views/login/LoginView.js';
 import RegisterView from 'Frontend/views/register/RegisterView';
 import ForgottenPasswordView from 'Frontend/views/forgotten-password/ForgottenPasswordView';
 import ResetForgottenPasswordView from 'Frontend/views/forgotten-password/ResetForgottenPasswordView';
+import HomeView from 'Frontend/views/home/HomeView';
+import ProfileView from 'Frontend/views/profile/ProfileView';
 import { createBrowserRouter, RouteObject } from 'react-router-dom';
 
 export const routes: RouteObject[] = [
-  {
-    element: (
-      <AuthControl fallback={<Placeholder />}>
-        <MainLayout />
-      </AuthControl>
-    ),
-    handle: { title: 'Main' },
-    children: [
-    ],
-  },
-  { path: '/login', element: <LoginView />, handle: { title: 'Log In', requiresLogin: false } },
-  { path: '/register', element: <RegisterView />, handle: { title: 'Register', requiresLogin: false } },
-  { path: '/forgotten-password', element: <ForgottenPasswordView />, handle: { title: 'Forgotten Password', requiresLogin: false } },
-  { path: '/reset-forgotten-password', element: <ResetForgottenPasswordView />, handle: { title: 'Reset Forgotten Password', requiresLogin: false } },
+    {
+        element: (
+        <AuthControl fallback={<Placeholder />}>
+            <MainLayout />
+        </AuthControl>
+        ),
+        handle: { title: 'Main' },
+        children: [
+            { path: '/', element: <HomeView />, handle: { title: 'Home', requiresLogin: true } },
+            { path: '/profile/:username', element: <ProfileView />, handle: { title: 'Profile', requiresLogin: true } },
+        ],
+    },
+    { path: '/login', element: <LoginView />, handle: { title: 'Log In', requiresLogin: false } },
+    { path: '/register', element: <RegisterView />, handle: { title: 'Register', requiresLogin: false } },
+    { path: '/forgotten-password', element: <ForgottenPasswordView />, handle: { title: 'Forgotten Password', requiresLogin: false } },
+    { path: '/reset-forgotten-password', element: <ResetForgottenPasswordView />, handle: { title: 'Reset Forgotten Password', requiresLogin: false } },
 ];
 
 export default createBrowserRouter(routes);
