b4d657fb147b0dbc9a22aa72a69d64f8ffeaf52a
Ádám Jakab <adam.jakab@lombiq.com>
2024-02-09 15:00:37 +0100

    Adding menu items to navbar and loading user profile; Adding functionality to be able to like posts, query them etc

--- frontend/components/navbar/Navbar.tsx
+++ frontend/components/navbar/Navbar.tsx
@@ -1,18 +1,65 @@
 import 'themes/intertwine/components/navbar.scss';
-import { Icon } from "@hilla/react-components/Icon.js";
+import MenuBarHelpers from 'Frontend/helpers/menuBarHelpers';
+import User from 'Frontend/generated/hu/eenugw/usermanagement/models/User';
+import UserProfile from 'Frontend/generated/hu/eenugw/userprofilemanagement/models/UserProfile';
+import { Icon } from '@hilla/react-components/Icon.js';
 import { Avatar } from '@hilla/react-components/Avatar.js';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from 'Frontend/util/auth';
+import { useEffect, useState } from 'react';
+import { SiteEndpoint, UserProfileEndpoint } from 'Frontend/generated/endpoints';
+import { MenuBar, } from '@hilla/react-components/MenuBar.js';
 
 export default function Navbar() {
-    const { state, logout } = useAuth();
     const assetsFolder = import.meta.env.VITE_ASSETS_FOLDER;
+
+    const { state, logout } = useAuth();
+    const [siteName, setSiteName] = useState<string>('');
+    const [userProfile, setUserProfile] = useState<UserProfile | undefined>();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        (async () => {
+            const siteName = await SiteEndpoint.getSiteName();
+            setSiteName(siteName);
+
+            const userProfile = await UserProfileEndpoint.getUserProfileById(state.user?.userProfileId!);
+            setUserProfile(userProfile);
+        })();
+    }, [state.user]);
+
+    const menuBarItems = [
+        {
+            component: MenuBarHelpers.renderMenuComponent(
+                <Avatar
+                    className='navbar-image'
+                    theme='xsmall'
+                    img={userProfile?.profilePicturePath ? assetsFolder + userProfile?.profilePicturePath : 'images/no-profile-picture.png'}
+                    name={state.user?.username} />
+            ),
+            children: [
+                {
+                    text: 'Profile',
+                },
+                {
+                    text: 'Settings',
+                },
+                {
+                    text: 'Help',
+                },
+                {
+                    text: 'Log out',
+                },
+            ],
+        },
+    ];
 
     return (
         <>
             <div slot='navbar' className='navbar-left'>
                 <Link to='/'>
-                    <span className='navbar-logo'>InterTwine</span>
+                    <span className='navbar-logo'>{siteName}</span>
                 </Link>
             </div>
             <div slot='navbar' className='navbar-center'>
@@ -23,8 +70,10 @@ export default function Navbar() {
             </div>
             <div slot='navbar' className='navbar-right'>
                 <div className='navbar-links'>
-                    <span className='navbar-link'>Homepage</span>
-                    <span className='navbar-link'>Timeline</span>
+                    <Link to='/'>
+                        <span className='navbar-link'>Homepage</span>
+                        <span className='navbar-link'>Timeline</span>
+                    </Link>
                 </div>
                 <div className='navbar-icons'>
                     <div className='navbar-icon-item'>
@@ -40,11 +89,34 @@ export default function Navbar() {
                         <span className='navbar-icon-badge'>1</span>
                     </div>
                 </div>
-                {state.user ? (
+                {/* {currentUser && (
                     <>
-                        <Avatar onClick={logout} className='navbar-image' theme="xsmall" img={assetsFolder + state.user?.userProfile?.profilePicturePath} name={state.user.username} />
+                        <MenuBar
+                            className='navbar-menu-bar'
+                            items={menuBarItems}
+                            theme="tertiary-inline"
+                            onItemSelected={(event) => {
+                                if (event.detail.value.text === 'Log out') {
+                                    logout();
+                                }
+                                if (event.detail.value.text === 'Profile') {
+                                    navigate(new URL(`profile/${userProfile?.profileDisplayId}`, document.baseURI).pathname);
+                                }
+                            }} />
                     </>
-                ) : null}
+                )} */}
+                <MenuBar
+                    className='navbar-menu-bar'
+                    items={menuBarItems}
+                    theme="tertiary-inline"
+                    onItemSelected={(event) => {
+                        if (event.detail.value.text === 'Log out') {
+                            logout();
+                        }
+                        if (event.detail.value.text === 'Profile') {
+                            navigate(new URL(`profile/${userProfile?.profileDisplayId}`, document.baseURI).pathname);
+                        }
+                    }} />
             </div>
         </>
     )
--- frontend/components/post/Post.tsx
+++ frontend/components/post/Post.tsx
@@ -1,59 +1,150 @@
 import 'themes/intertwine/components/post.scss';
-import UserProfilePost from 'Frontend/generated/hu/eenugw/userprofilemanagement/entities/UserProfilePost';
+import User from 'Frontend/generated/hu/eenugw/usermanagement/models/User';
+import UserProfile from 'Frontend/generated/hu/eenugw/userprofilemanagement/models/UserProfile';
+import UserProfilePost from 'Frontend/generated/hu/eenugw/userprofilemanagement/models/UserProfilePost';
+import ReactionType from 'Frontend/generated/hu/eenugw/userprofilemanagement/constants/ReactionType';
+import { Link } from 'react-router-dom';
 import { Avatar } from '@hilla/react-components/Avatar.js';
 import { Icon } from '@hilla/react-components/Icon.js';
-import { useState } from 'react';
-import UserProfile from 'Frontend/generated/hu/eenugw/userprofilemanagement/entities/UserProfile';
-import User from 'Frontend/generated/hu/eenugw/usermanagement/entities/User';
+import { useEffect, useState } from 'react';
 import { TDate, format } from 'timeago.js';
+import { UserProfileEndpoint, UserProfilePostEndpoint } from 'Frontend/generated/endpoints';
+import { DateTime } from 'luxon';
+import { useAuth } from 'Frontend/util/auth';
 
-export default function Post({post}: {post: UserProfilePost | null}) {
+export default function Post({userProfilePost}: {userProfilePost: UserProfilePost | undefined}) {
     const assetsFolder = import.meta.env.VITE_ASSETS_FOLDER;
-    const [like, setLike] = useState(post?.likeCount ?? 0);
-    const [heart, setHeart] = useState(post?.heartCount ?? 0);
+    const { state } = useAuth();
+    const [currentUser, setCurrentUser] = useState<User>();
+    const [userProfile, setUserProfile] = useState<UserProfile | undefined>();
+    const [likeCount, setLikeCount] = useState(0);
+    const [heartCount, setHeartCount] = useState(0);
+    const [commentCount, setCommentCount] = useState(0);
     const [isLiked, setIsLiked] = useState(false);
     const [isHearted, setIsHearted] = useState(false);
-    const [canLike, setCanLike] = useState(true);
-    const [canHeart, setCanHeart] = useState(true);
-    const [userProfile, setUserProfile] = useState<UserProfile | null>(post?.userProfile ?? null);
-    const [user, setUser] = useState<User | null>(userProfile?.user ?? null);
-    const [numberOfComments, setNumberOfComments] = useState(post?.userProfileComments?.length ?? 0);
+    const [creationDateLocal, setCreationDateLocal] = useState(DateTime.local().toISO() as string);
 
-    const likeHandler = () => {
-        setLike(isLiked ? like - 1 : like + 1);
-        setIsLiked(!isLiked);
+    useEffect(() => {
+        setLikeCount(userProfilePost?.userProfileLikeIds?.length ?? 0);
+        setHeartCount(userProfilePost?.userProfileHeartIds?.length ?? 0);
+        setCommentCount(userProfilePost?.userProfilePostCommentIds?.length ?? 0);
+        setIsLiked(userProfilePost?.userProfileLikeIds?.includes(currentUser?.userProfileId!) ?? false);
+        setIsHearted(userProfilePost?.userProfileHeartIds?.includes(currentUser?.userProfileId!) ?? false);
+    }, [currentUser, userProfilePost]);
+
+    const reactionHandler = async (reactionType: ReactionType) => {
+        try {
+            const result = await UserProfilePostEndpoint.likeDislikePost(userProfilePost?.id!, currentUser?.userProfileId!, reactionType);
+
+            console.log(result);
+
+            if (reactionType === ReactionType.LIKE) {
+                if (isHearted) {
+                    // User is hearting the post, remove heart and add like
+                    setHeartCount(heartCount - 1);
+                    setIsHearted(false);
+                    setLikeCount(likeCount + 1);
+                    setIsLiked(true);
+                } else {
+                    // User is liking the post, toggle like
+                    setLikeCount(isLiked ? likeCount - 1 : likeCount + 1);
+                    setIsLiked(!isLiked);
+                }
+            } else if (reactionType === ReactionType.HEART) {
+                if (isLiked) {
+                    // User is liking the post, remove like and add heart
+                    setLikeCount(likeCount - 1);
+                    setIsLiked(false);
+                    setHeartCount(heartCount + 1);
+                    setIsHearted(true);
+                } else {
+                    // User is hearting the post, toggle heart
+                    setHeartCount(isHearted ? heartCount - 1 : heartCount + 1);
+                    setIsHearted(!isHearted);
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
-    const heartHandler = () => {
-        setHeart(isHearted ? heart - 1 : heart + 1);
-        setIsHearted(!isHearted);
-    }
+    useEffect(() => {
+        (async () => {
+            if (!state.initializing) {
+                setCurrentUser(state?.user);
+            }
+
+            const userProfile = await UserProfileEndpoint.getUserProfileById(userProfilePost?.userProfileId!);
+            setUserProfile(userProfile);
+
+            const creationDateLocal = DateTime.fromISO(userProfilePost?.creationDateUtc as string, { zone: 'utc' }).setZone('local').toISO() as string;
+            setCreationDateLocal(creationDateLocal);
+        })();
+    }, [state, currentUser, userProfilePost]);
 
     return (
         <div className='post'>
             <div className='post-wrapper'>
                 <div className='post-top'>
                     <div className='post-top-left'>
-                        <Avatar theme='xsmall' className='post-top-left-image' img={userProfile?.profilePicturePath == undefined ? 'images/no-profile-picture.png' : assetsFolder + userProfile?.profilePicturePath} name='friend-avatar' />
-                        <span className='post-top-left-username'>{user?.username}</span>
-                        <span className='post-top-left-date'>{format(post?.creationDateUtc as TDate)}</span>
+                        <Link className='post-top-left-image-link' to={'/profile/' + userProfile?.profileDisplayId}>
+                            <Avatar
+                                theme='xsmall'
+                                className='post-top-left-image'
+                                img={userProfile?.profilePicturePath ? assetsFolder + userProfile?.profilePicturePath : 'images/no-profile-picture.png'}
+                                name='friend-avatar' />
+                        </Link>
+                        <Link className='post-top-left-name-link' to={'/profile/' + userProfile?.profileDisplayId}>
+                            <span className='post-top-left-name'>{userProfile?.fullName}</span>
+                        </Link>
+                        <span className='post-top-left-date'>{format(creationDateLocal as TDate)}</span>
                     </div>
                     <div className='post-top-right'>
                         <Icon className='post-top-right-icon fa fa-ellipsis-h' />
                     </div>
                 </div>
                 <div className='post-center'>
-                    <span className='post-center-text'>{post?.description}</span>
-                    <img className='post-center-image' src={assetsFolder + post?.photoPath} />
+                    <span className='post-center-text'>{userProfilePost?.description}</span>
+                    {userProfilePost?.photoPath && <img className='post-center-image' src={assetsFolder + userProfilePost?.photoPath} />}
                 </div>
                 <div className='post-bottom'>
                     <div className='post-bottom-left'>
-                        <img className='post-bottom-left-like-icon' src='images/like.png' onClick={likeHandler} />
-                        <img className='post-bottom-left-heart-icon' src='images/heart.png' onClick={heartHandler} />
-                        <span className='post-bottom-left-like-counter'>{like + heart} {like + heart <= 1 ? 'person likes it' : 'people like it'}</span>
+                        {/* {likeCount >= heartCount
+                            ? <img className='post-bottom-left-like-icon' src='images/like.png' onClick={() => reactionHandler(ReactionType.LIKE)} />
+                            : <img className='post-bottom-left-heart-icon' src='images/heart.png' onClick={() => reactionHandler(ReactionType.HEART)} />}
+                        {likeCount >= heartCount
+                            ? <img className='post-bottom-left-heart-icon' src='images/heart.png' onClick={() => reactionHandler(ReactionType.HEART)} />
+                            : <img className='post-bottom-left-like-icon' src='images/like.png' onClick={() => reactionHandler(ReactionType.LIKE)} />} */}
+                        {likeCount >= heartCount ? (
+                            <img
+                                className={isLiked ? 'post-bottom-left-like-icon active' : 'post-bottom-left-like-icon'}
+                                src='images/like.png'
+                                onClick={() => reactionHandler(ReactionType.LIKE)}
+                            />
+                        ) : (
+                            <img
+                                className={isHearted ? 'post-bottom-left-heart-icon active' : 'post-bottom-left-heart-icon'}
+                                src='images/heart.png'
+                                onClick={() => reactionHandler(ReactionType.HEART)}
+                            />
+                        )}
+                        {likeCount >= heartCount ? (
+                            <img
+                                className={isHearted ? 'post-bottom-left-heart-icon active' : 'post-bottom-left-heart-icon'}
+                                src='images/heart.png'
+                                onClick={() => reactionHandler(ReactionType.HEART)}
+                            />
+                        ) : (
+                            <img
+                                className={isLiked ? 'post-bottom-left-like-icon active' : 'post-bottom-left-like-icon'}
+                                src='images/like.png'
+                                onClick={() => reactionHandler(ReactionType.LIKE)}
+                            />
+                        )}
+                        <span className='post-bottom-left-like-counter'>{likeCount + heartCount} {likeCount + heartCount <= 1 ? 'person likes it' : 'people like it'}</span>
                     </div>
                     <div className='post-bottom-right'>
-                        <span className="post-bottom-right-comment-text">{numberOfComments} {numberOfComments <= 1 ? 'comment' : 'comments'}</span>
+                        <span className="post-bottom-right-comment-text">{commentCount} {commentCount <= 1 ? 'comment' : 'comments'}</span>
                     </div>
                 </div>
             </div>
