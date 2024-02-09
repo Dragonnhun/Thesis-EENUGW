0f1aad963eb964b3535febc3b769eb38597c9603
Ádám Jakab <adam.jakab@lombiq.com>
2024-02-09 14:59:18 +0100

    Scss changes; Adding ProfileFriend component

---
+++ frontend/components/profile-friend/ProfileFriend.tsx
@@ -0,0 +1,16 @@
+import 'themes/intertwine/components/profile-friend.scss';
+import UserProfile from 'Frontend/generated/hu/eenugw/userprofilemanagement/models/UserProfile';
+import { Link } from 'react-router-dom';
+
+export default function ProfileFriend({userProfile}: {userProfile?: UserProfile}) {
+    const assetsFolder = import.meta.env.VITE_ASSETS_FOLDER;
+
+    return (
+        <Link className='rightbar-user-friends-followings-following-link' to={'/profile/' + userProfile?.profileDisplayId}>
+            <div className='rightbar-user-friends-followings-following'>
+                <img className='rightbar-user-friends-followings-following-image' src={userProfile?.coverPicturePath ? assetsFolder + userProfile?.coverPicturePath : 'images/no-cover-picture.png'} alt='user-following' />
+                <span className='rightbar-user-friends-followings-following-name'>{userProfile?.fullName}</span>
+            </div>
+        </Link>
+    )
+}
