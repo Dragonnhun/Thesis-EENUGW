470ef9f3dfde514d286b67fcdf85c84206dfe6a1
Ádám Jakab <adam.jakab@lombiq.com>
2024-03-17 19:18:01 +0100

    Using strict mode in development environment

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,17 +1,26 @@
 import router from 'Frontend/routes.js';
-// import React from 'react';
+import React from 'react';
 import { AuthProvider } from 'Frontend/util/auth.js';
 import { RouterProvider } from 'react-router-dom';
 
 export default function App() {
+    const environment = import.meta.env.VITE_SITE_ENVIRONMENT as string;
+
     return (
-        // <React.StrictMode>
-        //     <AuthProvider>
-        //         <RouterProvider router={router} />
-        //     </AuthProvider>
-        // </React.StrictMode>
-        <AuthProvider>
-            <RouterProvider router={router} />
-        </AuthProvider>
+        <>
+            {
+                environment === 'DEVELOPMENT'
+                    ?
+                        <React.StrictMode>
+                            <AuthProvider>
+                                <RouterProvider router={router} />
+                            </AuthProvider>
+                        </React.StrictMode>
+                    :
+                        <AuthProvider>
+                            <RouterProvider router={router} />
+                        </AuthProvider>
+            }
+        </>
     );
 }
