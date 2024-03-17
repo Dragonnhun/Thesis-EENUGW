099d133817f5971a82b1cde055b8d3a0f421b03c
Ádám Jakab <adam.jakab@lombiq.com>
2024-03-17 20:33:38 +0100

    Adding logging levels, logging endpoint and service

---
+++ src/main/java/hu/eenugw/core/services/LoggerService.java
@@ -0,0 +1,35 @@
+package hu.eenugw.core.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import hu.eenugw.core.constants.LogType;
+
+@Service
+public class LoggerService {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerService.class);
+
+    public void log(String message, LogType logType) {
+        switch (logType) {
+            case DEBUG:
+                logger.debug(message);
+                break;
+            case ERROR:
+                logger.error(message);
+                break;
+            case INFO:
+                logger.info(message);
+                break;
+            case TRACE:
+                logger.trace(message);
+                break;
+            case WARN:
+                logger.warn(message);
+                break;
+            default:
+                logger.info(message);
+                break;
+        }
+    }
+}
